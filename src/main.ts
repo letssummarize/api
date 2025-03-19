73ba8ccb6cff571763b496b0b3e534c7b91080c8
Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>
2025-03-19 21:11:01 +0300

    feat: add AWS Lambda support (WIP)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Callback, Context, Handler } from 'aws-lambda';
+import serverlessExpress from '@vendia/serverless-express';
+
+let cachedServer: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,5 +14,19 @@ async function bootstrap() {
   });
 
   await app.listen(process.env.PORT ?? 3000);
+  return app;
 }
-bootstrap();
+
+// Run locally only if not in Lambda
+if (!process.env.AWS_LAMBDA_FUNCTION_NAME) {
+  bootstrap();
+}
+
+export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
+  if (!cachedServer) {
+    const app = await bootstrap();
+    const expressApp = app.getHttpAdapter().getInstance();
+    cachedServer = serverlessExpress({ app: expressApp });
+  }
+  return cachedServer(event, context, callback);
+};
