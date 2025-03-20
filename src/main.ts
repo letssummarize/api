b74df42521de680e34a1f902e380a1575365056f
Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>
2025-03-20 15:11:34 +0300

    prod: setup vercel for deployment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { Express } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,19 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  if (process.env.NODE_ENV === 'production') {
+    await app.init();
+    const expressApp = app.getHttpAdapter().getInstance();
+    return expressApp;
+  } else {
+    await app.listen(process.env.PORT ?? 3000);
+  }
 }
-bootstrap();
+
+// Run locally in development
+if (process.env.NODE_ENV !== 'production') {
+  bootstrap();
+}
+
+// Export for Vercel in production
+export default bootstrap;
