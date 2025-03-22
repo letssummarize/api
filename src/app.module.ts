4f0b32a3e9c9e8a4d7565c5820dbdd3f25e4ef37
Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>
2025-03-22 14:58:19 +0300

    prod: deploy to vercel (WIP)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,15 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { SummarizationModule } from './summarization/summarization.module';
 import { ScheduleModule } from '@nestjs/schedule';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
-import { PUBLIC_DIR } from './utils/constants';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'downloads'),
-      serveRoot: PUBLIC_DIR,
-    }),
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(__dirname, '..', 'downloads'),
+    //   serveRoot: PUBLIC_DIR,
+    // }),
     SummarizationModule
   ],
   controllers: [AppController],
